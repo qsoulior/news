2ed840ccf80d361844dd081afdc23db07fdafd11
Vladimir <74735618+qsoulior@users.noreply.github.com>
2024-03-24 23:09:00 +0300

    ria-parser: implement html parser

--- ria-parser/internal/service/news.go
+++ ria-parser/internal/service/news.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net/http"
 	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/brianvoe/gofakeit/v7"
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/proto"
 	"github.com/qsoulior/news/aggregator/entity"
@@ -28,7 +30,72 @@ type news struct {
 }
 
 func (n *news) parseOne(ctx context.Context, url string) (*entity.News, error) {
-	return nil, nil
+	resp, err := n.client.Get(ctx, url, map[string]string{
+		"User-Agent": gofakeit.UserAgent(),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("n.client.Get: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, newStatusError(resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("goquery.NewDocumentFromReader: %w", err)
+	}
+
+	news := &entity.News{
+		Source: n.appID,
+		Link:   resp.Request.URL.String(),
+	}
+
+	article := doc.Find(".article")
+
+	news.Categories = article.
+		Find(".article__supertag-header .article__supertag-header-title").
+		Map(func(i int, s *goquery.Selection) string { return s.Text() })
+
+	datetimeLayout := "15:04 02.01.2006"
+	datetimeStr := article.Find(".article__info-date-modified").Text()
+	if datetimeStr == "" {
+		datetimeStr = article.Find(".article__info-date a").Text()
+	} else {
+		parts := strings.SplitN(datetimeStr, " ", 2)
+		if len(parts) > 1 {
+			datetimeStr = strings.Trim(parts[1], "()")
+		}
+	}
+
+	news.PublishedAt, err = time.Parse(datetimeLayout, datetimeStr)
+	if err != nil {
+		return nil, fmt.Errorf("time.Parse: %w", err)
+	}
+
+	news.Title = article.Find(".article__title").Text()
+	news.Description = article.Find(".article__second-title").Text()
+
+	news.Authors = article.
+		Find(".article__author .article__author-name").
+		Map(func(i int, s *goquery.Selection) string { return s.Text() })
+
+	var text strings.Builder
+	textItems := article.Find(".article__body .article__text, .article__body .article__quote-text")
+	textItems.Each(func(i int, s *goquery.Selection) {
+		text.WriteString(s.Text())
+		if i < textItems.Size()-1 {
+			text.WriteRune('\n')
+		}
+	})
+	news.Content = text.String()
+
+	news.Tags = article.
+		Find(".article__tags a").
+		Map(func(i int, s *goquery.Selection) string { return s.Text() })
+
+	return news, nil
 }
 
 func (n *news) parseView(page *rod.Page, limit int) ([]string, error) {
